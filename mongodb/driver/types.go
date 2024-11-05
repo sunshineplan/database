c4833f6a856be858e26334c63c3f91e23ef0a31a
sunshineplan <sunshineplan@users.noreply.github.com>
2024-11-05 22:41:06 +0800

    mongodb: Rewrite ObjectID and Time interface (#59)

--- mongodb/driver/types.go
+++ mongodb/driver/types.go
@@ -1,7 +1,6 @@
 package driver
 
 import (
-	"encoding/json"
 	"time"
 
 	"github.com/sunshineplan/database/mongodb"
@@ -9,46 +8,14 @@ import (
 )
 
 var (
-	_ mongodb.ObjectID = objectID{bson.NilObjectID}
-	_ mongodb.Date     = date(time.Time{})
+	_ mongodb.ObjectID = bson.NilObjectID
+	_ mongodb.Time     = bson.DateTime(0)
 )
 
 func (*Client) ObjectID(s string) (mongodb.ObjectID, error) {
-	oid, err := bson.ObjectIDFromHex(s)
-	if err != nil {
-		return nil, err
-	}
-	return objectID{oid}, nil
+	return bson.ObjectIDFromHex(s)
 }
 
-type objectID struct{ bson.ObjectID }
-
-func (oid objectID) MarshalBSONValue() (typ byte, data []byte, err error) {
-	b, err := oid.MarshalJSON()
-	if err != nil {
-		return
-	}
-	return byte(bson.TypeObjectID), b, nil
-}
-
-type date time.Time
-
-func (d date) Time() time.Time {
-	return time.Time(d)
-}
-
-func (d date) MarshalJSON() ([]byte, error) {
-	return json.Marshal(time.Time(d))
-}
-
-func (d date) MarshalBSONValue() (typ byte, data []byte, err error) {
-	b, err := d.MarshalJSON()
-	if err != nil {
-		return
-	}
-	return byte(bson.TypeDateTime), b, nil
-}
-
-func (*Client) Date(t time.Time) mongodb.Date {
-	return date(t)
+func (*Client) Time(t time.Time) mongodb.Time {
+	return bson.NewDateTimeFromTime(t)
 }
